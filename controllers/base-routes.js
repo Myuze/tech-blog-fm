1c9cb0b34fdeed9ca7481fcf560f1df14d8efadb
Myuze <Myuze@users.noreply.github.com>
2021-10-18 00:59:47 -0700

    Added baseroute, some dependencies, and handlebars

---
+++ controllers/base-routes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { Blog } = require('../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const blogData = Blog.findAll();
+
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
+    res.status(200).render('home', { blogs });
+  } catch (err) {
+    console.log(err)
+    res.status(500).render('404');
+  }
+});
+
+module.exports = router;
---
+++ models/User.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {};
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
+);
+
+module.exports = User;

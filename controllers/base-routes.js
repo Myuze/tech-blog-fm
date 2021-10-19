66db2120312550029e410fc71e27c5bf5d458f59
Florian Meneses <florian.meneses@gmail.com>
2021-10-18 17:15:33 -0700

    Added Welcome handlebars, landing banner

--- controllers/base-routes.js
+++ controllers/base-routes.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
 const { Blog } = require('../models');
 
+const withAuth = require('../utils/auth');
+
 router.get('/', async (req, res) => {
+  try {
+    res.status(200).render('welcome');
+  } catch (err) {
+    console.log(err);
+    res.status(500).render('404');
+  }
+});
+
+router.get('/blog', withAuth, async (req, res) => {
   try {
     const blogData = await Blog.findAll();
 
@@ -13,6 +24,10 @@ router.get('/', async (req, res) => {
     console.log(err)
     res.status(500).render('404');
   }
+});
+
+router.get('/login', async (req, res) => {
+
 });
 
 module.exports = router;
--- public/js/home.js
+++ public/js/home.js
@@ -1,4 +1,9 @@
+// Home page scripts
+// Menu Dropdown
 var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'));
 var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
   return new bootstrap.Dropdown(dropdownToggleEl)
 });
+
+// Blog Posts
+
